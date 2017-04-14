41cd3745fed7f56b61f59e4b81cef4a8cf579bd9
Mark Harvey <markedwinharvey@gmail.com>
2017-04-14 05:42:35 +0200

    initial

---
+++ alphacss.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+''' Alphabetize css files 
+
+Usage:
+	
+	$ python alphacss.py filename
+	
+'''
+import sys
+import os
+import re
+
+def main():
+	if len(sys.argv) > 1:
+		filepath = sys.argv[1]
+	with open(filepath,'r') as f:
+		doc = f.read()
+	
+	el_dict = {}
+	els = re.findall('[^{}]*\{[^{}]*\}',doc)
+	
+	
+	for entry in els: 
+		el = re.search('\w+[^{]*',entry)
+		if el in el_dict:
+			print 'Warning: Found multiple instances of:',el
+			sys.exit()
+		el_dict[el.group()] = entry
+	
+	print sorted(el_dict, key=lambda x: x.lower())
+	doc = ''.join( [el_dict[y] for y in sorted(el_dict, key=lambda x: x.lower())] )
+	with open(filepath,'w') as f:
+		f.write(doc)
+	
+	
+	pass
+if __name__ == '__main__':
+	main()
