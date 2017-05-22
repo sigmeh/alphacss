add501bff5b8eaa2d562736ec8abd4af23cc6cf3
Mark Harvey <markedwinharvey@gmail.com>
2017-05-22 02:41:07 +0200

    fix selector-separation & selector-agnostic alphabetization

--- alphacss.py
+++ alphacss.py
@@ -18,17 +18,19 @@ def main():
 	
 	el_dict = {}
 	els = re.findall('[^{}]*\{[^{}]*\}',doc)
-	
+	#print els
 	
 	for entry in els: 
-		el = re.search('\w+[^{]*',entry)
+		el = re.search('.*\w+[^{]*',entry)
 		if el in el_dict:
 			print 'Warning: Found multiple instances of:',el
 			sys.exit()
 		el_dict[el.group()] = entry
 	
-	print sorted(el_dict, key=lambda x: x.lower())
-	doc = ''.join( [el_dict[y] for y in sorted(el_dict, key=lambda x: x.lower())] )
+	#print sorted(el_dict, key=lambda x: re.search('\w.*',x.lower() ).group() )
+	
+	doc = ''.join( [el_dict[y] for y in sorted(el_dict, key=lambda x: re.search('\w+.*',x.lower()).group()) ] )
+	
 	with open(filepath,'w') as f:
 		f.write(doc)
 	
