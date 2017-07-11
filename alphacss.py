e905ba54a8b3c5fd236c76877644213cba831244
sigmeh <markh@tuta.io>
2017-07-11 20:09:19 +0200

    handle absent filename and Nonetype element; save original css file

--- alphacss.py
+++ alphacss.py
@@ -12,9 +12,16 @@ import re
 
 def main():
 	if len(sys.argv) > 1:
-		filepath = sys.argv[1]
-	with open(filepath,'r') as f:
+		filename = sys.argv[1]
+	else:
+		print 'Supply filename as argument'
+		sys.exit()
+		
+	with open(filename,'r') as f:
 		doc = f.read()
+	
+	with open(filename + '_original', 'w') as f:	#save original css file
+		f.write(doc)
 	
 	el_dict = {}
 	els = re.findall('[^{}]*\{[^{}]*\}',doc)
@@ -22,19 +29,20 @@ def main():
 	
 	for entry in els: 
 		el = re.search('.*\w+[^{]*',entry)
+		if not el: continue
 		if el in el_dict:
 			print 'Warning: Found multiple instances of:',el
 			sys.exit()
+			
 		el_dict[el.group()] = entry
 	
 	#print sorted(el_dict, key=lambda x: re.search('\w.*',x.lower() ).group() )
 	
 	doc = ''.join( [el_dict[y] for y in sorted(el_dict, key=lambda x: re.search('\w+.*',x.lower()).group()) ] )
 	
-	with open(filepath,'w') as f:
+	with open(filename, 'w') as f:
 		f.write(doc)
-	
-	
-	pass
+
+
 if __name__ == '__main__':
 	main()
